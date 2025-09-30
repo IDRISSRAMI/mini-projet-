d358c3aedc513e91fbd4c52da7b15d90b2622fb8
idriss rami <rdriss306@gmail.com>
2025-09-30 16:07:55 +0100

    tiket routes

---
+++ backend/routes/ticket.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/auth.middleware");
+const adminMiddleware = require("../middleware/admin.midddleware");
+const {
+    createTicket,
+    getAllTickets,
+    getTicketById,
+    updateTicket,
+    deleteTicket,
+} = require("../controllers/ticket.controller");
+
+router.use(authMiddleware);
+
+router.post("/cree", createTicket);
+router.get("/getall", getAllTickets);
+router.get("/get/:id", getTicketById);
+router.put("/modifier/:id", updateTicket);
+router.delete("/suppreme/:id", deleteTicket);
+
+module.exports = router;
